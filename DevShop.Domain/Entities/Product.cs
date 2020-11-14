05c89e8d4222f5c760c68fd49ca6cdbbbca8c010
Gustavo de Miranda <gustavolpu@hotmail.com>
2020-11-13 22:33:30 -0300

    Add project files.

---
+++ DevShop.Domain/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevShop.Domain.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Photo { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ DevShop.Infrastructure.DataAccess/DevShopDbContext.cs
@@ -0,0 +1,24 @@
+﻿using DevShop.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevShop.Infrastructure.DataAccess
+{
+    public class DevShopDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DevShopDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DevShopDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
